d07f1f29907ce9f780df18985e9ce70c7006d3d2
wtindall1 <wtindall@hotmail.com>
2023-09-22 22:43:40 +0100

    Created db context and db model class

---
+++ Sequence_Pro.Application/Database/SequenceProContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sequence_Pro.Application.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequence_Pro.Application.Database;
+public class SequenceProContext : DbContext
+{
+    public DbSet<SequenceAnalysis> SequenceAnalyses { get; set; }
+    
+    public SequenceProContext(DbContextOptions<SequenceProContext> options) 
+        : base(options)
+    {
+    }
+}
--- Sequence_Pro.Application/Repositories/SequenceAnalysisRepository.cs
+++ Sequence_Pro.Application/Repositories/SequenceAnalysisRepository.cs
@@ -1,4 +1,6 @@
-﻿using Sequence_Pro.Application.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using Sequence_Pro.Application.Interfaces;
+using Sequence_Pro.Application.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,34 @@ using System.Threading.Tasks;
 namespace Sequence_Pro.Application.Repositories;
 public class SequenceAnalysisRepository : ISequenceAnalysisRepository
 {
+    private readonly DbContext _dbContext;
+    public SequenceAnalysisRepository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
 
+    public Task<bool> CreateAsync(SequenceAnalysis sequenceAnalysis, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<SequenceAnalysis>> GetAllAsync(CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SequenceAnalysis?> GetByIdAsync(Guid id, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SequenceAnalysis?> GetByUniprotIdAsync(string uniprotId, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
 }
